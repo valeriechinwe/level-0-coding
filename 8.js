ac4bce3758b414389c98028188cf40f21f9bcc3a
valeriechinwe <v.c.k.a@outlook.com>
2021-10-20 16:54:03 +0200

    updated logic

--- 8.js
+++ 8.js
@@ -1,7 +1,20 @@
 function toHours(x) {
   var hours = Math.floor(x / 60);
   var minutes = x % 60;
-  return hours + ":" + minutes;
+
+if (hours > 1 && minutes > 1) {
+  return hours + " hours, " + minutes + " minutes";
+
+} else if (hours > 1 && minutes <= 1) {
+    return hours + " hours, " + minutes + " minute";
+
+} else if (hours <= 1 && minutes > 1) {
+  return hours + " hour, " + minutes + " minutes";
+
+} else {
+  return hours + " hour, " + minutes + " minute";
 }
+}
+
 // test
-console.log(toHours(90));
+console.log(toHours(71));
